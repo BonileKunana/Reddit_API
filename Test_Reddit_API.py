f285ce1f016e3f014cc32b27ee60c09e2ef5a8b6
Bonile Kunana <kunanabonile@gmail.com>
2024-03-27 21:52:52 +0200

    Vote down test cases added

--- Test_Reddit_API.py
+++ Test_Reddit_API.py
@@ -6,8 +6,10 @@ base_url = 'http://localhost:5000'
 
 
 # Define a list of test cases for each endpoint
+
+#positive test cases
 positive_test_cases = [
-    #positive test cases
+    
     ('POST', '/post', {'title': 'Test Post', 'content': 'This is a test post', 'author': 'test_user'}, 200, 'success'),
     ('PUT', '/post/1', {'title': 'Updated Post', 'content': 'This post has been updated'}, 200, 'success'),
     ('POST', '/post/1/vote', {'vote': 'up'}, 200, 'success'),
@@ -95,9 +97,12 @@ positive_test_cases = [
 ]
 '''
  ),
-     ('DELETE', '/post/1', None, 200, 'success'),
+    ('POST', '/comment/1/vote=[]', {'vote': 'down'}, 200, 'success'),
+    ('DELETE', '/post/1', None, 200, 'success'),
+]
 
-    # negetive test cases
+# negetive test cases
+negetive_test_cases = [
     ('PUT', '/post/100', {'title': 'Updated Post', 'content': 'This post has been updated'}, 404, 'not found'),
     ('POST', '/post/100/vote', {'vote': 'up'}, 404, 'not found'),
     ('POST', '/post/100/comment', {'author': 'test_user', 'content': 'This is a test comment'}, 404, 'not found'),
@@ -107,23 +112,30 @@ positive_test_cases = [
     ('GET', '/user/non_user/votes', None, 404, 'not found'),
     ('GET', '/post/100/comments', None, 404, 'not found'),
     ('DELETE', '/post/1', None, 404, 'not found'),
-
 ]
 
-# Iterate over each test case and make requests to the server
+# Iterate over each test case in positive test cases and make requests to the server
+print("Positive test cases:")
 for method, endpoint, data, expected_status, expected_response in positive_test_cases:
     url = base_url + endpoint
     response = requests.request(method, url, json=data)
     actual_status = response.status_code
     actual_response = response.text
-    #print(actual_response)
-    """try:
-        actual_response_dict = json.loads(actual_response)
-    except json.JSONDecodeError as e:
-        print("Error parsing JSON:", e)"""
     
     if actual_status == expected_status and actual_response == expected_response:
         print(f"Test case for {method} {endpoint}: PASSED")
     else:
         print(f"Test case for {method} {endpoint}: FAILED. Expected status {expected_status}, response '{expected_response}', but got status {actual_status}, response '{actual_response}'") 
-    #print(actual_response)
+print("")
+print("Negetive test cases:")
+
+# Iterate over each test case in negetive test cases and make requests to the server
+for method, endpoint, data, expected_status, expected_response in negetive_test_cases:
+    url = base_url + endpoint
+    response = requests.request(method, url, json=data)
+    actual_status = response.status_code
+    actual_response = response.text
+    if actual_status == expected_status and actual_response == expected_response:
+        print(f"Test case for {method} {endpoint}: PASSED")
+    else:
+        print(f"Test case for {method} {endpoint}: FAILED. Expected status {expected_status}, response '{expected_response}', but got status {actual_status}, response '{actual_response}'") 
